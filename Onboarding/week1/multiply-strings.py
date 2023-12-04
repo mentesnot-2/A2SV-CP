31a80fd84e5e9084e4dc0cef091bc694c651506e
Mentesnot Sibatu <113641638+mentesnot-2@users.noreply.github.com>
2023-12-04 17:25:10 +0300

    Added solution for Multiply Strings

---
+++ Onboarding/week1/multiply-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        num3 = int(num1)
+        num4 = int(num2)
+        val = num3 * num4
+        return str(val)
+        
