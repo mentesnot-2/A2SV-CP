607041c00dc52461edb41553a6497ccf324d175f
Mentesnot Sibatu <113641638+mentesnot-2@users.noreply.github.com>
2024-01-03 18:08:20 +0300

    Added solution for Count Pairs Whose Sum is Less than Target

--- Regural/week2/count-pairs-whose-sum-is-less-than-target.py
+++ Regural/week2/count-pairs-whose-sum-is-less-than-target.py
@@ -1,11 +1,14 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
-        n = len(nums)
+        nums.sort()
         count = 0
-        for i in range(n -1):
-            for j in range(i+1,n):
-                if nums[i] + nums[j] < target:
-                    count+=1
-        return count
+        l,r = 0,len(nums) - 1
 
+        while l < r:
+            if nums[l] + nums[r] < target:
+                count+=(r -l)
+                l+=1
+            else:
+                r-=1
+        return count
         
