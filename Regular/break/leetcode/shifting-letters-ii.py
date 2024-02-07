5f3d5cd5b467d720a3624a19f4bd7ab8108b20c6
Mentesnot Sibatu <113641638+mentesnot-2@users.noreply.github.com>
2024-02-07 16:11:18 +0300

    Add solution for Shifting Letters II

--- Regular/break/leetcode/shifting-letters-ii.py
+++ Regular/break/leetcode/shifting-letters-ii.py
@@ -1,24 +1,25 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        pref = [0]*(len(s) + 1)
-
+        prefix = [0 for _ in range(len(s) + 1)]
         for shift in shifts:
-            start,end,dirn = shift
-            if dirn == 1:
-                pref[start] += 1
-                pref[end + 1] -= 1
-            else:
-                pref[start] -= 1
-                pref[end + 1] += 1
-        for indx in range(1, len(pref)):
-            pref[indx] += pref[indx - 1]
+            l, r, dr = shift
+            prefix[l] +=(1 if dr == 1 else -1)
 
-        ans = ""
+            prefix[r + 1]-=(1 if dr == 1 else -1)
+
+
+            
+
+        pref = [prefix[0]]
+        for i in range(1,len(prefix)):
+            pref.append(pref[-1] + prefix[i])
         
+        
+        ans = []
         for i in range(len(s)):
-            _ord = ord(s[i]) - ord('a')
-
-            val = (_ord + pref[i]) % 26
-            ans += chr(val + ord('a')) 
-
-        return ans
+            curr = ord(s[i]) - ord("a")
+            curr+=pref[i]
+            curr %=26
+            curr+=(ord("a"))
+            ans.append(chr(curr))
+        return "".join(ans)
