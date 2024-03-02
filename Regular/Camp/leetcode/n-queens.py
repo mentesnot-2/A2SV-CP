45360fb9b185f071c43155969b7b357eec75eb97
Mentesnot Sibatu <113641638+mentesnot-2@users.noreply.github.com>
2024-03-02 15:24:14 +0300

    Add solution for N-Queens

---
+++ Regular/Camp/leetcode/n-queens.py
@@ -0,0 +1,20 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for _ in range(n)]
+        col = [0] * n
+        diag = [0] * (2 * n)
+        oppDiagn = [0] * (2 * n)
+        ans = []
+        def backtrack(row):
+            if row == n:
+                ans.append(["".join(curr_state) for curr_state in board])
+            for c in range(n):
+                if col[c] + diag[row + c] + oppDiagn[n - row + c] == 0:
+                    board[row][c] = "Q"
+                    col[c] = diag[c  + row] = oppDiagn[n - row + c] = 1
+                    backtrack(row + 1)
+                    col[c] = diag[c  + row] = oppDiagn[n - row + c] = 0
+                    board[row][c] = "."
+            
+        backtrack(0)
+        return ans
